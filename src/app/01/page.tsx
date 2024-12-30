b73383657c317a86856d5dc9b38ca63a2a59d343
aaron <aaron.a@me.com>
2024-12-29 22:54:32 -0600

    initial project setup
    Signed-off-by: aaron <aaron.a@me.com>

---
+++ src/app/01/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+      <div className="">
+        <h1>JANUARY 01</h1>
+      </div>
+    );
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1>GENUARY</h1>
+      <Link href="/01" >01</Link>
+    </div>
+  );
+}
