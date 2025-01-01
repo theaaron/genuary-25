791153fe14414bac8987017637760840c89508e6
aaron <aaron.a@me.com>
2024-12-31 22:55:31 -0600

    added grid layout
    Signed-off-by: aaron <aaron.a@me.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
+import '@/app/globals.css';  // If in app directory
 
 export default function Home() {
   return (
     <div className="">
-      <h1>GENUARY</h1>
-      <Link href="/01" >01</Link>
+      <div className="main-title source-code-pro-800">
+        <h1>GENUARY</h1>
+      </div>
+      <div className="grid grid-cols-3 md:grid-cols-5 gap-4 max-w-4xl mx-auto">
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        <Link href="/01" className="grid-item" >01</Link>
+        </div>
     </div>
   );
 }
