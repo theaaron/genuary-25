06e48c3df4ae5951d452c991e915ec38482ccf97
aaron <aaron.a@me.com>
2025-01-02 00:11:33 -0600

    set up for next day.
    Signed-off-by: aaron <aaron.a@me.com>

---
+++ src/app/02/Sketch02.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useEffect, useRef } from 'react'
+import p5 from 'p5'
+
+export default function Sketch02() {
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+    const sketch = (p: p5) => {
+     
+      p.setup = () => {
+        const canvas = p.createCanvas(600, 400)
+        canvas.parent(containerRef.current!)
+      }
+
+      p.draw = () => {
+        p.background(30);
+       
+      }
+
+     
+
+
+
+    }
+
+    const p5Instance = new p5(sketch)
+    return () => p5Instance.remove()
+  }, [])
+
+  return <div ref={containerRef}></div>
+}
