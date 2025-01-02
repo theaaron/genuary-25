ec99b7fd90484d7ac53c557b91982298863d11fe
aaron <aaron.a@me.com>
2025-01-02 00:11:10 -0600

    updated styling
    Signed-off-by: aaron <aaron.a@me.com>

--- src/app/01/page.tsx
+++ src/app/01/page.tsx
@@ -1,12 +1,16 @@
+import Link from 'next/link';
 import ClientP5Wrapper from './ClientP5Wrapper'
 import '@/app/globals.css';
 
 export default function Page() {
   return (
     <div>
-      <div className="main-title">
-        <h1>GENUARY</h1>
+      <div className="title-area">
+        <Link href="/" >
+          <h1 className='main-title' >GENUARY</h1>
+        </Link>
         <h2 className='date-title'>25.01.01</h2>
+        <h4 className='prompt-desc'>Prompt: Vertical or horizontal lines only.</h4>
       </div>
 
       <ClientP5Wrapper />
