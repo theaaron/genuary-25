cc1148b6391945f5e5594dbd572a51997230b7a7
aaron <aaron.a@me.com>
2025-01-02 00:11:52 -0600

    removed placeholders
    Signed-off-by: aaron <aaron.a@me.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,12 @@ import '@/app/globals.css';
 export default function Home() {
   return (
     <div>
-      <div className="main-title source-code-pro-800">
-        <h1>GENUARY</h1>
+      <div className="title-area">
+        <h1 className="main-title">GENUARY 2025</h1>
       </div>
       <div className="grid grid-cols-3 md:grid-cols-5 gap-4 max-w-4xl mx-auto p-4">
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        {/* <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
-        <Link href="/01" className="aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link> */}
+        <Link href="/01" className="grid-item aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">01</Link>
+        <Link href="/02" className="grid-item aspect-square w-full flex items-center justify-center bg-gray-100 hover:bg-gray-200 rounded-lg transition-colors">02</Link>
       </div>
     </div>
   );
